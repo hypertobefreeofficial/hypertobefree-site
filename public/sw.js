ce27a730714ab5a0f4fbdc101a6c40d3ac961971
hypertobefreeofficial <info@hypertobefree.com>
2026-05-27 21:06:20 -0700

    add service worker

---
+++ public/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "htbf-cache-v1";
+
+const STATIC_ASSETS = [
+  "/",
+  "/share-your-story",
+  "/account",
+  "/images/icons/icon-192.png",
+  "/images/icons/icon-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
