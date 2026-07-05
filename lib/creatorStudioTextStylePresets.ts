540e01718d46e2fa2124e3da9becd7b51555dc71
hypertobefreeofficial <info@hypertobefree.com>
2026-07-05 10:24:33 -0700

    Update creatorStudioTextStylePresets.ts

--- lib/creatorStudioTextStylePresets.ts
+++ lib/creatorStudioTextStylePresets.ts
@@ -1,1 +1,42 @@
-export {};
+import type { CreatorStudioLayerStyle } from "./creationCenter";
+import {
+  creatorStudioFontPresets,
+  getCreatorStudioFontPresetDefinition,
+  type CreatorStudioFontPreset,
+  type CreatorStudioFontPresetDefinition,
+} from "./creatorStudioTypography";
+
+export type CreatorStudioTextStylePreset = CreatorStudioFontPresetDefinition;
+
+export const creatorStudioTextStylePresets: CreatorStudioTextStylePreset[] =
+  creatorStudioFontPresets;
+
+export function applyCreatorStudioFontPreset(
+  fontPreset: CreatorStudioFontPreset
+): Partial<CreatorStudioLayerStyle> {
+  const preset = getCreatorStudioFontPresetDefinition(fontPreset);
+  if (!preset) {
+    return { fontPreset };
+  }
+
+  return {
+    fontPreset: preset.value,
+    weight: preset.weight,
+    italic: preset.italic,
+    fontSize: preset.fontSize,
+    fontScale: preset.fontScale,
+    letterSpacing: preset.letterSpacing,
+    lineHeight: preset.lineHeight,
+    shadowStrength: preset.shadowStrength,
+    outlineWidth: preset.outlineWidth,
+    textTransform: preset.textTransform,
+    color: preset.defaultColor,
+  };
+}
+
+/** @deprecated Use applyCreatorStudioFontPreset */
+export function applyCreatorStudioTextStylePreset(
+  fontPreset: CreatorStudioFontPreset
+) {
+  return applyCreatorStudioFontPreset(fontPreset);
+}
