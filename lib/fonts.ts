0c92749276a2196b3550f480348a479bdaedbeb1
hypertobefreeofficial <info@hypertobefree.com>
2026-07-05 14:47:15 -0700

    Update fonts.ts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,27 +1,37 @@
-import { EB_Garamond, Funnel_Display, Inter, Manrope } from "next/font/google";
+import {
+  EB_Garamond,
+  Funnel_Display,
+  Inter,
+  Manrope,
+} from "next/font/google";
 
 export const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
   display: "swap",
+  weight: ["400", "500", "600", "700"],
 });
 
 export const manrope = Manrope({
   subsets: ["latin"],
   variable: "--font-manrope",
   display: "swap",
+  weight: ["500", "600", "700", "800"],
 });
 
 export const funnelDisplay = Funnel_Display({
   subsets: ["latin"],
   variable: "--font-funnel-display",
   display: "swap",
+  weight: ["600", "700", "800"],
 });
 
 export const ebGaramond = EB_Garamond({
   subsets: ["latin"],
   variable: "--font-eb-garamond",
   display: "swap",
+  weight: ["400", "500", "600", "700"],
+  style: ["normal", "italic"],
 });
 
 export const fontVariables = [
