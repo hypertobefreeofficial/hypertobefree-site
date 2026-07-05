9ce5b0cdf1a26da2eec42a22c0582e13bba4b956
hypertobefreeofficial <info@hypertobefree.com>
2026-07-05 15:19:00 -0700

    Update fonts.ts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,8 +1,15 @@
 import {
+  Bebas_Neue,
+  Caveat,
+  Caveat_Brush,
   EB_Garamond,
   Funnel_Display,
+  Great_Vibes,
   Inter,
   Manrope,
+  Oswald,
+  Playfair_Display,
+  Special_Elite,
 } from "next/font/google";
 
 export const inter = Inter({
@@ -34,9 +41,66 @@ export const ebGaramond = EB_Garamond({
   style: ["normal", "italic"],
 });
 
+export const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  variable: "--font-playfair-display",
+  display: "swap",
+  weight: ["600", "700"],
+  style: ["normal", "italic"],
+});
+
+export const bebasNeue = Bebas_Neue({
+  subsets: ["latin"],
+  variable: "--font-bebas-neue",
+  display: "swap",
+  weight: "400",
+});
+
+export const oswald = Oswald({
+  subsets: ["latin"],
+  variable: "--font-oswald",
+  display: "swap",
+  weight: ["500", "600", "700"],
+});
+
+export const caveat = Caveat({
+  subsets: ["latin"],
+  variable: "--font-caveat",
+  display: "swap",
+  weight: ["500", "600", "700"],
+});
+
+export const caveatBrush = Caveat_Brush({
+  subsets: ["latin"],
+  variable: "--font-caveat-brush",
+  display: "swap",
+  weight: "400",
+});
+
+export const greatVibes = Great_Vibes({
+  subsets: ["latin"],
+  variable: "--font-great-vibes",
+  display: "swap",
+  weight: "400",
+});
+
+export const specialElite = Special_Elite({
+  subsets: ["latin"],
+  variable: "--font-special-elite",
+  display: "swap",
+  weight: "400",
+});
+
 export const fontVariables = [
   inter.variable,
   manrope.variable,
   funnelDisplay.variable,
   ebGaramond.variable,
+  playfairDisplay.variable,
+  bebasNeue.variable,
+  oswald.variable,
+  caveat.variable,
+  caveatBrush.variable,
+  greatVibes.variable,
+  specialElite.variable,
 ].join(" ");
